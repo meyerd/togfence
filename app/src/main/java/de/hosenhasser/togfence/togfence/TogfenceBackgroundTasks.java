bd7d635488082e7a702e8e2cffb073ecb6f07485
Dominik <meyerd@mytum.de>
2019-05-05 16:37:41 +0200

    add regularly remove and add geofences

--- app/src/main/java/de/hosenhasser/togfence/togfence/TogfenceBackgroundTasks.java
+++ app/src/main/java/de/hosenhasser/togfence/togfence/TogfenceBackgroundTasks.java
@@ -4,7 +4,9 @@ import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import de.hosenhasser.togfence.togfence.Toggl.TogglRetrofit;
 
@@ -12,20 +14,36 @@ public class TogfenceBackgroundTasks extends JobService {
     private static final String PACKAGE_NAME = "de.hosenhasser.togfence.togfence";
     static final String MAIN_SHOWN_KEY = PACKAGE_NAME + ".MAIN_SHOWN_KEY";
 
+    private static final String TAG = "TogfenceBackgroundTasks";
+
     public TogfenceBackgroundTasks() {
     }
 
     @Override
     public boolean onStartJob(JobParameters params) {
-        Context context = getApplicationContext();
+        final Context context = getApplicationContext();
 
 
         Boolean mainShown = PreferenceManager.getDefaultSharedPreferences(
                 TogfenceApplication.getAppContext())
                 .getBoolean(MAIN_SHOWN_KEY, false);
+        Boolean autostartGeofences = PreferenceManager.getDefaultSharedPreferences(
+                TogfenceApplication.getAppContext())
+                .getBoolean("autostart_geofencing_on_boot", false);
 //        if (mainShown) {
 //            UpdateTogglInformation.startUpdateTogglInformation(context);
 //        }
+        if(mainShown && autostartGeofences) {
+            Log.i(TAG, "removing and adding geofences");
+            GeofencesManagerService.startActionStopGeofencing(context);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    GeofencesManagerService.startActionStartGeofencing(context);
+                }
+            }, 1000);
+        }
         StartBackgroundTasksOnBootReceiver.scheduleTogfenceBackgroundTasks(context);
         return true;
     }
